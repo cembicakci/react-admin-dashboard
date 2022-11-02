aaf9fdaab677d1437b18b65ef9a7287ac1fe4c2c
Cem <cmbicakci@gmail.com>
2022-11-02 19:40:16 +0300

    Recharts

---
+++ src/dummyData.js
@@ -0,0 +1,74 @@
+export const userData = [
+    {
+        name: 'Jan',
+        'ActiveUser': 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Feb',
+        'ActiveUser': 3000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Mar',
+        'ActiveUser': 2000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Apr',
+        'ActiveUser': 3000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'May',
+        'ActiveUser': 2000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Jun',
+        'ActiveUser': 3000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Jul',
+        'ActiveUser': 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Aug',
+        'ActiveUser': 2000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Sep',
+        'ActiveUser': 1000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Oct',
+        'ActiveUser': 3000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Nov',
+        'ActiveUser': 2000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Dec',
+        'ActiveUser': 1000,
+        pv: 2400,
+        amt: 2400,
+    },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './Home.css'
+import { userData } from '../../dummyData'
 
 function Home() {
+
   return (
     <div className='home'>
-        <FeaturedInfo />
+      <FeaturedInfo />
+      <Chart data={userData} title='User Analytics' grid dataKey='ActiveUser' />
     </div>
   )
 }
