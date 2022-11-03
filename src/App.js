686984d061b66758da28b11755fb6655b10bdb7a
Cem <cmbicakci@gmail.com>
2022-11-03 18:26:45 +0300

    table for users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
+        </Routes>
+
       </div>
-    </div>
+    </Router>
   );
 }
 
